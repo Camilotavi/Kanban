cc62c529273a3fd1c84c8fb401874104d9e47b63
camil <camilotrzn@gmail.com>
2024-03-06 13:23:43 -0500

    Inicio proyecto

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class HelloWorld {
+    private JPanel rootPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloWorld");
+        frame.setContentPane(new HelloWorld().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
